23767dd4095c2f5b82480f3b4189fbb7f2e22865
Wes Garner <wesgarner@gmail.com>
2011-01-01 13:59:29 -0600

    espresso: Board uses Froyo libCamera

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -72,6 +72,8 @@ BOARD_USE_NASTY_PTHREAD_CREATE_HACK=true
 
 BOARD_USE_NEW_LIBRIL_HTC := true
 
+BOARD_USE_FROYO_LIBCAMERA := true
+
 # # cat /proc/mtd
 # dev:    size   erasesize  name
 # mtd0: 000a0000 00020000 "misc"
